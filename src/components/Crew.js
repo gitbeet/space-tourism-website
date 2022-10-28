d26be354b4fdc3dc28e490a46397529a49835cc7
gitbeet <nikig715@gmail.com>
2022-10-28 19:39:29 +0200

    added transitions between planet and crew images other small visual fixes

--- src/components/Crew.js
+++ src/components/Crew.js
@@ -1,45 +1,63 @@
-import React, { useEffect, useState } from 'react'
-import '../css/Crew.css'
-import data from '../data.json'
-import Background from './Background'
+import React, { useEffect, useState } from "react";
+import "../css/Crew.css";
+import data from "../data.json";
+import Background from "./Background";
 
 function Crew() {
+  const [crewMember, setCrewMember] = useState("Douglas Hurley");
+  const [image, setImage] = useState();
 
-  const [crewMember,setCrewMember] = useState("Douglas Hurley")
-  const [image,setImage] = useState()
+  const currentCrewMember = data["crew"].find(
+    (member) => member.name === crewMember
+  );
+  const nameArray = crewMember.toLowerCase().split(" ");
 
-  const currentCrewMember = data['crew'].find(member => member.name === crewMember)
-  const nameArray = crewMember.toLowerCase().split(' ')
-  
   useEffect(() => {
-    import(`../assets/crew/image-${nameArray[0]}-${nameArray[1]}.png`).then( image => setImage(image.default))
-  },[crewMember])
-
- 
-
+    import(`../assets/crew/image-${nameArray[0]}-${nameArray[1]}.png`).then(
+      (image) => setImage(image.default)
+    );
+  }, [crewMember]);
 
   return (
-    <div className='container'>
-      <Background page={'crew'}/>
+    <div className="container">
+      <Background page={"crew"} />
       <div class="wrapper">
         <div className="crew-container">
           <div class="image-container">
-            <img className='crew-image' src={image} alt='crew member' />
+            <img
+              key={`planet-${crewMember}`}
+              className="crew-image"
+              src={image}
+              alt="crew member"
+            />
           </div>
-          <div className='crew-text'>
-            <div className='button-container'>
-              {data['crew'].map(member => <div onClick={() => setCrewMember(member.name)} className={`${member.name === crewMember ? 'button-dot-active button-dot' : 'button-dot'}`}></div>)}
+          <div className="crew-text">
+            <div className="button-container">
+              {data["crew"].map((member) => (
+                <div
+                  onClick={() => setCrewMember(member.name)}
+                  className={`${
+                    member.name === crewMember
+                      ? "button-dot-active button-dot"
+                      : "button-dot"
+                  }`}
+                ></div>
+              ))}
             </div>
             <div class="crew-info">
-              <h4 className='crew-role'>{currentCrewMember.role.toUpperCase()}</h4>
-              <h5 className='crew-name font-one color-white'>{currentCrewMember.name.toUpperCase()}</h5>
-              <p className='crew-bio text-center'>{currentCrewMember.bio}</p>
+              <h4 className="crew-role">
+                {currentCrewMember.role.toUpperCase()}
+              </h4>
+              <h5 className="crew-name font-one color-white">
+                {currentCrewMember.name.toUpperCase()}
+              </h5>
+              <p className="crew-bio text-center">{currentCrewMember.bio}</p>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Crew
+export default Crew;
--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,50 +1,92 @@
-import React, { useEffect, useState } from 'react'
-import '../css/Destination.css'
-import data from '../data.json'
-import Background from './Background'
-
-
+import React, { useEffect, useState } from "react";
+import "../css/Destination.css";
+import data from "../data.json";
+import Background from "./Background";
 
 function Destination() {
+  const [destination, setDestination] = useState("Moon");
+  const [image, setImage] = useState();
 
-  const [destination,setDestination] = useState('Moon')
-  const [image,setImage] = useState()
-
-  let currentDestination = data['destinations'].find(dest => dest.name === destination)
+  let currentDestination = data["destinations"].find(
+    (dest) => dest.name === destination
+  );
 
   useEffect(() => {
-    import(`../assets/destination/image-${destination.toLocaleLowerCase()}.png`).then( image => setImage(image.default))
-  },[destination]) 
+    import(`../assets/destination/image-${destination.toLowerCase()}.png`).then(
+      (image) => setImage(image.default)
+    );
+  }, [destination]);
 
   return (
-    <div className='container'>
-      <Background page={'destination'}/>
+    <div className="container">
+      <Background page={"destination"} />
       <div class="wrapper">
-        <img className='planet-image' src={image} alt='destination' />
-        <div className='planet-text-wrapper'>
-          <div className='planet-text'>
-            <div className='button-container'>
-              {data['destinations'].map(dest => <button
-                                                    className={`${dest.name === destination ? 'button active' : 'button'}`}
-                                                    onClick={() => setDestination(dest.name)}>{dest.name.toUpperCase()}</button>)}
+        {/* {
+          data["destinations"].map((d) => {
+            return (
+              <img
+                className={
+                  d.name === destination ? "planet-image-show" : "planet-image"
+                }
+                src={image}
+                alt="destination"
+              />
+            );
+          })
+        } */}
+        <img
+          key={`planet-${destination}`}
+          className={
+            currentDestination === destination
+              ? "planet-image-show"
+              : "planet-image"
+          }
+          src={image}
+          alt="destination"
+        />
+        <div className="planet-text-wrapper">
+          <div className="planet-text">
+            <div className="button-container">
+              {data["destinations"].map((dest) => (
+                <button
+                  className={`${
+                    dest.name === destination ? "button active" : "button"
+                  }`}
+                  onClick={() => setDestination(dest.name)}
+                >
+                  {dest.name.toUpperCase()}
+                </button>
+              ))}
             </div>
-            <h3 className='planet-name color-white text-uppercase'>{currentDestination.name}</h3>
-            <p className='planet-description border-bottom text-center'>{currentDestination.description}</p>
-            <div className='planet-tablet-footer'>
+            <h3 className="planet-name color-white text-uppercase">
+              {currentDestination.name}
+            </h3>
+            <p className="planet-description border-bottom text-center">
+              {currentDestination.description}
+            </p>
+            <div className="planet-tablet-footer">
               <div>
-                <h5 className='destination-small-text subheading-two text-center'>AVG.DISTANCE</h5>
-                <h5 className='destination-medium-text font-one color-white text-center'>{currentDestination.distance}</h5>
+                <h5 className="destination-small-text subheading-two text-center">
+                  AVG.DISTANCE
+                </h5>
+                <h5 className="destination-medium-text font-one color-white text-center">
+                  {currentDestination.distance}
+                </h5>
               </div>
               <div>
-                <h5 className='destination-small-text subheading-two text-center'>EST. TRAVEL TIME</h5>
-                <h5 className='destination-medium-text text-uppercase font-one color-white text-center'>{currentDestination.travel}</h5>
+                <h5 className="destination-small-text subheading-two text-center">
+                  EST. TRAVEL TIME
+                </h5>
+                <h5 className="destination-medium-text text-uppercase font-one color-white text-center">
+                  {currentDestination.travel}
+                </h5>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Destination
+export default Destination;
